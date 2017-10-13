1484015f3626e3d95db572405c598045b8862e78
Satyaki Ghosh <satyakig901@hotmail.com>
2017-10-13 15:05:54 -0600

    no message

--- Lab2/sum.cpp
+++ Lab2/sum.cpp
@@ -14,33 +14,53 @@
 #include <string>
 #include <fstream>
 #include <pthread.h>
+#include <fstream>
 
 using namespace std;
 
 static int sum = 0;
 static int arr[1000000];
 
-void * thread_print(void * tid) {
+void* thread_print(void * tid) {
     printf("thread %ld running\n", tid);
     pthread_exit(0);
-}
-  
+}  
 
 int main(int argc, char **argv){
-    pthread_t threads[NUM_THREADS];
-    int rc;
-    int i;
     
-    for( i = 0; i < NUM_THREADS; i++ ) {
-       cout << "main() : creating thread, " << i << endl;
-       rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
-       
-       if (rc) {
-          cout << "Error:unable to create thread," << rc << endl;
-          exit(-1);
-       }
+    if(argc != 3) {
+        fprintf(stderr, "This program needs 2 arguments to run. The name of the file with integers and the number of threads to use\n");
+        exit(-1);
     }
-    pthread_exit(NULL);
 
+    char* file = argv[1];
+    int max_threads = atoi(argv[2]);
 
+    ifstream read(file);
+    int num, total = 0;
+    while(read >> num) {
+        arr[total] = num;
+        total++;
+    }
+
+    cout << "Total: " << total << endl;
+    for(int i = 0; i <= total; i++)
+        cout << arr[i] << endl;
+
+    // pthread_t threads[max_threads];
+    // int rc;
+    // int i;
+    
+    // for( i = 0; i < max_threads; i++ ) {
+    //    cout << "main() : creating thread, " << i << endl;
+    //    rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
+       
+    //    if (rc) {
+    //       cout << "Error:unable to create thread," << rc << endl;
+    //       exit(-1);
+    //    }
+    // }
+    // pthread_exit(NULL);
+
+    return 0;
 }
