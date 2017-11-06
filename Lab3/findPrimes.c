dcdce794401dc2f63cdba7bbee3c11d8d394a90e
Satyaki Ghosh <satyakig901@hotmail.com>
2017-11-05 18:13:54 -0700

    no message

--- Lab3/findPrimes.c
+++ Lab3/findPrimes.c
@@ -36,7 +36,7 @@ void* isPrime(void* tid)
     if(splits[id] == arrSize)
         i = splits[id - 1];
 
-    printf("Thread %d: start[%d] = %d  end[%d] = %d\n", id, i, nums[i], splits[id], nums[splits[id]-1]);
+    printf("Thread %d: start[%d] = %" PRId64"  end[%d] = %" PRId64 "\n", id, i, nums[i], splits[id], nums[splits[id]-1]);
 
     for( ; i < splits[id]; i++) {
         int64_t num = nums[i];
@@ -67,7 +67,7 @@ void* isPrime(void* tid)
             pthread_mutex_unlock(&lock);
         }
         
-        printf("Thread %d: %" PRId64 "is %s\n", id, num, prime? "prime" : "not prime");
+        printf("Thread %d: %" PRId64 " is %s\n", id, num, prime? "prime" : "not prime");
     }
 
     pthread_exit(0);
