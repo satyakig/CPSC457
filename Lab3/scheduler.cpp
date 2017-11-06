e469668c984992b34e5c3ee84aad8c0c0edb090e
Satyaki Ghosh <satyakig901@hotmail.com>
2017-11-05 22:27:33 -0700

    no message

--- Lab3/scheduler.cpp
+++ Lab3/scheduler.cpp
@@ -11,15 +11,70 @@
  *  $ g++ scheduler.cpp
  *********************************************/
 
- #include <iostream>
- #include <string>
- #include <fstream>
- #include <vector> 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <queue> 
   
- using namespace std;
+using namespace std;
 
- int main(int argc, char **argv)
- {
+int minIndex(vector<int> vec) {
+    int index = 0;
+    for(int i = 1; i < vec.size(); i++) {
+        if(vec.at(index) > vec.at(i))
+            index = i;
+    }
+
+    return index;
+}
+
+int findFirstSJF(vector<int> arrival, vector<int> burst) {
+    int index = 0;
+    int bur = burst.at(0);
+    
+    for(int i = 1; i < arrival.size() && arrival.at(0) == arrival.at(i); i++) {
+        if(bur > burst.at(i)) {
+            bur = burst.at(i);
+            index = i;
+        }
+    }
+
+    return index;
+}
+
+int printStart(vector<int> arrival) {
+    printf("Time");
+    for(int i = 0; i < arrival.size(); i++)
+        printf(" P%d ", i);
+    cout << endl;
+    printf("-------------------------------------------------------------------------------------------------\n");
+
+    if(arrival.at(0) == 0) {        
+        return 0;
+    }
+    else {
+        int i;
+        for(i = 0; i < arrival.at(0); i++) {
+            printf("%d\n", i);
+        }
+        return i;
+    }
+}
+
+void SJF(vector<int> arrival, vector<int> burst) {
+    queue<int> que;
+    que.push(findFirstSJF(arrival, burst));
+
+    int arrIndex = 0;
+
+    for(int i = printStart(arrival); que.size() != 0; i++) {
+        printf("%d    .  .  .\n", i);
+    }
+}
+
+int main(int argc, char **argv)
+{
     if(argc < 3) {
         printf("Configuration file name and scheduling algorithm required to run!\n");
         exit(-1);
@@ -29,10 +84,10 @@
     string command = argv[2];
     int quantum = 0; 
     
-    vector <int> arrival;   
-    vector <int> burst;  
+    vector<int> arrival;   
+    vector<int> burst;  
 
-    if(command.compare("SFJ") == 0 || command.compare("RR") == 0) {
+    if(command.compare("SJF") == 0 || command.compare("RR") == 0) {
         if(command.compare("RR") == 0) {
             if(argc != 4) {
                 printf("You need to specify the time-quantum for RR scheduling.\n");
@@ -47,16 +102,12 @@
             }                
         }
         else if(argc == 4)
-            printf("Time-quantum specified for SFJ algorithm but it is ignored.\n");
+            printf("Time-quantum specified for SJF algorithm but it is ignored.\n");
     }
     else {
-        printf("Invalid scheduling algorithm, only SFJ and RR accepted.\n");
+        printf("Invalid scheduling algorithm, only SJF and RR accepted.\n");
         exit(-1);
     }
-
-    cout << config << endl;
-    cout << command << endl;
-    cout << quantum << endl;
     
     ifstream file(config);
     if(!file){
@@ -70,12 +121,8 @@
         burst.push_back(b);
     }
 
-    for(int i = 0; i < arrival.size(); i++) {
-        cout << arrival.at(i) << " " << burst.at(i) << endl;
-    }
-
-    cout << "Size: " << arrival.size() << " " << burst.size() << endl;
-
+    if(command.compare("SJF") == 0)
+        SJF(arrival, burst);
+    
     return 0;
-
- }
+}
